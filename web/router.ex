d735016959ceab8b690fb42baefbe8bbd597d3bf
Luis Vasconcellos <luis@luisvasconcellos.com>
2017-12-22 20:34:57 -0200

    Add TopicController RESTful features

---
+++ web/router.ex
@@ -0,0 +1,21 @@
+defmodule Discuss.Router do
+  use Discuss.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Discuss do
+    pipe_through :browser
+
+    resources "/", TopicController
+  end
+end
